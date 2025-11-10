ac3bb63711d574674a66248f4eeed3b666116833
MinhBadGuy <D3etMe4n@gmail.com>
2025-11-10 17:55:15 +0700

    Added minio for files upload, user profile and upload endpoint

--- backend/init-script/01-schema.sql
+++ backend/init-script/01-schema.sql
@@ -1,18 +1,26 @@
-CREATE TABLE IF NOT EXISTS users(
-	id UUID PRIMARY KEY DEFAULT uuidv7(),
-	username VARCHAR(50) UNIQUE NOT NULL,
-	email VARCHAR(255) NOT NULL,
-	password VARCHAR(255) NOT NULL,
-	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS refresh_token (
+CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT uuidv7(),
-    user_id UUID NOT NULL,
-    token VARCHAR(500) NOT NULL UNIQUE,
-    expiry_date TIMESTAMP NOT NULL,
-    CONSTRAINT fk_refresh_token_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    phone_number VARCHAR(20) UNIQUE,
+    password_hash VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
-CREATE INDEX idx_refresh_token_token ON refresh_token(token);
-CREATE INDEX idx_refresh_token_user_id ON refresh_token(user_id);
+CREATE TABLE user_profile (
+    user_id UUID PRIMARY KEY,
+    field_of_study VARCHAR(100),
+    birth_date VARCHAR(255),
+    years_of_experience INTEGER DEFAULT 0,
+    organization VARCHAR(100),
+    position VARCHAR(100),
+    preferred_location VARCHAR(100),
+    avatar_path VARCHAR(300),
+    cv_path VARCHAR(300),
+    min_salary INTEGER,
+    max_salary INTEGER,
+    CONSTRAINT fk_user_profile_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- Create indexes for better query performance
+CREATE INDEX idx_users_username ON users(username);
--- backend/init-script/02-permission.sql
+++ backend/init-script/02-permission.sql
@@ -3,5 +3,5 @@ CREATE USER spring_boot_backend WITH PASSWORD 'thisisspringbootpassword';
 GRANT CONNECT ON DATABASE jobcubator_application_database to spring_boot_backend;
 GRANT USAGE ON SCHEMA PUBLIC TO spring_boot_backend;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE users TO spring_boot_backend;
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE refresh_token TO spring_boot_backend;
+GRANT SELECT, INSERT, UPDATE ON TABLE users TO spring_boot_backend;
+GRANT SELECT, INSERT, UPDATE ON TABLE user_profile TO spring_boot_backend;
