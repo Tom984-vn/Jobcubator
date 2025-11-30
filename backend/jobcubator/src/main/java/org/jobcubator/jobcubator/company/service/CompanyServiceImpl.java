8a04ebe4da0c78b2cd065a1c620d086c3e612efb
Vũ Nguyễn Đức Minh <111348629+D3etMe4n@users.noreply.github.com>
2025-11-30 17:01:23 +0700

    Change company to candidate

--- backend/jobcubator/src/main/java/org/jobcubator/jobcubator/company/service/CompanyServiceImpl.java
+++ backend/jobcubator/src/main/java/org/jobcubator/jobcubator/company/service/CompanyServiceImpl.java
@@ -74,7 +74,7 @@ public class CompanyServiceImpl implements CompanyService{
         company.addMember(currentUser, CompanyRole.OWNER);
         company = companyRepository.save(company);
 
-        if(currentUser.getRole() == Role.COMPANY){
+        if(currentUser.getRole() == Role.CANDIDATE){
             currentUser.setRole(Role.COMPANY);
             userRepository.save(currentUser);
         }
